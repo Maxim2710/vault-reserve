bb400dccf4bdff24618f9ef78d2d6c08a6d185f4
PrimakMV <pm2710@mail.ru>
2025-02-25 22:28:27 +0300

    [VR-B-K-010] The logic of checking the availability of the required quantity of goods in the service has been implemented

--- inventory/src/main/kotlin/com/inventory/dto/event/reserved/InventoryReservedEvent.kt
+++ inventory/src/main/kotlin/com/inventory/dto/event/reserved/InventoryReservedEvent.kt
@@ -2,5 +2,5 @@ package com.inventory.dto.event.reserved
 
 data class InventoryReservedEvent(
     val orderId: Long,
-    val reservedItem: List<ReservedItem>
+    val reservedItems: List<ReservedItem>
 )
