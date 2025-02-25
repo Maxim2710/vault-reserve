68e2be2d848eaaea2fabde4ef3ea927f7ae0ea1d
PrimakMV <pm2710@mail.ru>
2025-02-25 13:42:53 +0300

    [VR-B-K-003] The necessary models have been created for creating orders and interacting with them.

---
+++ order/src/main/kotlin/com/order/model/OrderItem.kt
@@ -0,0 +1,23 @@
+package com.order.model
+
+import jakarta.persistence.*
+
+@Entity
+@Table(name = "order_items")
+data class OrderItem (
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    val id: Long = 0,
+
+    @Column(name = "product_id", nullable = false)
+    val productId: Long,
+
+    @Column(name = "quantity", nullable = false)
+    val quantity: Int,
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "order_id", nullable = false)
+    @Column(name = "order")
+    val order: Order
+)
